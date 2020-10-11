18facd365131a0a00a0e74a25229bb722668f27a
nabd0018 <nabd0018@student.monash.edu>
2020-10-11 15:08:31 +0200

    Adding the new classes

---
+++ src/game/Grass.java
@@ -0,0 +1,14 @@
+package game;
+
+import edu.monash.fit2099.engine.Ground;
+
+public class Grass extends Ground {
+    /**
+     * Constructor.
+     *
+     * @param displayChar character to display for this type of terrain
+     */
+    public Grass(char displayChar) {
+        super(displayChar);
+    }
+}
