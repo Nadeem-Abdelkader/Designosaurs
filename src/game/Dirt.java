d4236621ed405a74e024dd33d682dccf070656bf
nabd0018 <nabd0018@student.monash.edu>
2020-10-11 18:55:47 +0200

    Intial implementation of Dirt Growing Grass

--- src/game/Dirt.java
+++ src/game/Dirt.java
@@ -1,13 +1,21 @@
 package game;
 
 import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
+import game.Grass;
 
 /**
  * A class that represents bare dirt.
  */
 public class Dirt extends Ground {
 
+	Grass grass = new Grass();
+
 	public Dirt() {
 		super('.');
+	}
+
+	public void growGrass(Location location){
+		grass.tick(location);
 	}
 }
