471b331060982124198b59d543358b2b29b48f51
nabd0018 <nabd0018@student.monash.edu>
2020-10-12 13:55:08 +0200

    Updating Grass.java

--- src/game/Grass.java
+++ src/game/Grass.java
@@ -1,23 +1,10 @@
 package game;
 
 import edu.monash.fit2099.engine.Ground;
-import edu.monash.fit2099.engine.Location;
 
 public class Grass extends Ground {
-    private int age = 0;
 
     public Grass() {
-        super('+');
-    }
-
-    @Override
-    public void tick(Location location) {
-        super.tick(location);
-
-        age++;
-        if (age == 10)
-            displayChar = 'g';
-        if (age == 20)
-            displayChar = 'G';
+        super('G');
     }
 }
