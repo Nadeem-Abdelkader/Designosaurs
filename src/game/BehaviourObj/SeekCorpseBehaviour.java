57ebf1692fb7287b961e9adcd63192f5b2dc58d4
nabd0018 <nabd0018@student.monash.edu>
2020-11-06 13:30:21 +0200

    updating SeekCorpseBehaviour.java

--- src/game/BehaviourObj/SeekCorpseBehaviour.java
+++ src/game/BehaviourObj/SeekCorpseBehaviour.java
@@ -1,12 +1,37 @@
 package game.BehaviourObj;
 
-import edu.monash.fit2099.engine.Action;
-import edu.monash.fit2099.engine.Actor;
-import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.*;
+import game.GroundObj.Water;
 
 public class SeekCorpseBehaviour implements Behaviour {
+    private final Actor target;
+
+    public SeekCorpseBehaviour(Actor subject) {
+        this.target = subject;
+    }
+
     @Override
     public Action getAction(Actor actor, GameMap map) {
+        if (!map.contains(target) || !map.contains(actor))
+            return null;
+
+        Location here = map.locationOf(actor);
+        Location there = map.locationOf(target);
+        int currentDistance = distance(here, there);
+
+        for (Exit exit : here.getExits()) {
+            Location destination = exit.getDestination();
+            if (destination.canActorEnter(actor)) {
+                int newDistance = distance(destination, there);
+                if (newDistance < currentDistance) {
+                    return new MoveActorAction(destination, exit.getName());
+                }
+            }
+        }
+
         return null;
+    }
+    private int distance(Location a, Location b) {
+        return Math.abs(a.x() - b.x()) + Math.abs(a.y() - b.y());
     }
 }
